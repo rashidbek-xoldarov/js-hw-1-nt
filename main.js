b2150688ff52c5c6fad4fe83c0996ff7fbe25b67
rashidbek <rashidbek0309@gmail.com>
2022-09-19 17:35:38 +0500

    Finish

---
+++ main.js
@@ -0,0 +1,38 @@
+const input = document.querySelectorAll(".form-input");
+const title = document.querySelectorAll(".info-content");
+const elBtn = document.querySelector(".form-btn");
+const elResetBtn = document.querySelector(".info-btn");
+
+elBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  let work = false;
+  for (i = 0; i < input.length; i++) {
+    if (input[i].value) {
+      work = true;
+    } else {
+      work = false;
+    }
+  }
+  if (work) {
+    input.forEach((item, index) => {
+      if (index == 4) {
+        console.log(item.value);
+        let sum = item.value * 11000;
+        title[index].textContent = sum;
+      } else {
+        title[index].textContent = item.value;
+      }
+    });
+  } else {
+    alert("Please, fill all blank!!!");
+  }
+  if (work) {
+    input.forEach((item) => {
+      item.value = "";
+    });
+  }
+});
+
+elResetBtn.addEventListener("click", function () {
+  location.reload();
+});
